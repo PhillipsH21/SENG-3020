a73651e3c2e3653c32ed7982c3229e4b73c413ca
PhillipsH21 <89491746+PhillipsH21@users.noreply.github.com>
2023-09-13 21:25:43 -0400

    Add files via upload

--- eost/InsuranceTest.java
+++ eost/InsuranceTest.java
@@ -20,18 +20,28 @@ public class InsuranceTest {
 
     // Method to return the EP test data
     @DataProvider(name="dataset1")
-    public Object[][] getTestData() {
+    public static Object[][] getTestData() {
        return testData1;
     }
 
     // Method to execute the EP tests
     @Test(dataProvider="dataset1")
-    public void test_premium( String id, int age, Status ncb, boolean lowRisk, int expected)
+    public static void test_premium( String id, int age, Status ncb, boolean lowRisk, int expected)
     {
        assertEquals( Insurance.premium( age, ncb, lowRisk ), expected );
     }
 
+    /*
     public static void main(String[] args) {
-      System.out.println("Testing testing");
+      System.out.println(getTestData()[0][0]);
+
+      String temp_01 = (String)getTestData()[0][0];
+      int temp_02 = (int)getTestData()[0][1];
+      Status temp_03 = (Status)getTestData()[0][2];
+      boolean temp_04 = (boolean)getTestData()[0][3];
+      int temp_05 = (int)getTestData()[0][4];
+
+      test_premium(temp_01, temp_02, temp_03, temp_04, temp_05);
     }
+    */
 }
